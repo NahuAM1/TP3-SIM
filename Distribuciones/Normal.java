c8f1a61a673e6a4f2f22fb469352247db42b0c2a
Nicolas Tittarelli <risas888@gmail.com>
2022-04-12 21:06:40 -0300

    normal

--- Distribuciones/Normal.java
+++ Distribuciones/Normal.java
@@ -1,1 +1,49 @@
-System.out.printl()
+import java.util.ArrayList;
+
+public class Normal{
+
+	private double media;
+	private double desviacion;
+	private ArrayList<Double> serie;
+	private int cantintervalos;
+	private int N_muestra;
+
+
+public double get media(){return media;}
+public double get desviacion(){return desviacion;}
+public ArrayList<Double> serie(){return serie;}
+public void setSerie(ArrayList<Double> serie) {
+        this.serie = serie;
+    }
+ public int getcantIntervalos() {
+        return this.cantintervalos;
+    }
+ public void setCantIntervalos(int cantIntervalos) {
+        this.cantIntervalos = cantIntervalos;
+    }
+public ArrayList<Double> getSerie() {
+        return this.serie;
+    }
+
+public int get N_muestra(){return N_muestra;}
+
+
+
+public void Fe(ArrayList<Double> serie,int cantIntervalos){
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
