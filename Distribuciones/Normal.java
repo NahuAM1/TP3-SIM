f67945eecc0398ce51ac0ccfde3ecc7a4b69cb93
Nicolas Tittarelli <risas888@gmail.com>
2022-04-15 19:09:29 -0300

    box muller mejorado

--- Distribuciones/Normal.java
+++ Distribuciones/Normal.java
@@ -4,7 +4,6 @@ public class Normal{
 
 	private double media;
 	private double desviacion;
-	private ArrayList<Double> serieRND;
 	private int cantintervalos;
 	private int N_muestra;
 
@@ -24,14 +23,7 @@ public class Normal{
         this.desviacion = desviacion;
     }
 
-    public ArrayList<Double> getSerieRND() {
-        return serieRND;
-    }
-
-    public void setSerieRND(ArrayList<Double> serieRND) {
-        this.serieRND = serieRND;
-    }
-
+    
     public int getCantintervalos() {
         return cantintervalos;
     }
@@ -50,36 +42,28 @@ public class Normal{
 
 
 
-     public ArrayList<Double> GenerarSecuencia(ArrayList<Double> serieRND, double desviacion, double media){
+     public ArrayList<Double> GenerarSecuenciaBoxMuller(double N_muestra, double desviacion, double media){
 
 
         ArrayList<Double> secuencia = new ArrayList<>();
 
-        for (int i = 0;i < this.serieRND.size();i++) {
-            if (i % 2 == 0) {
-                double rnd1 = serieRND.get(i);
-                double rnd2 = serieRND.get(i+1);
-                double N = Math.sqrt(-2 * Math.log(rnd1)) * Math.cos(2 * Math.PI * rnd2) * desviacion * media;
-                double N2 = Math.sqrt(-2 * Math.log(rnd1)) * Math.sin(2 * Math.PI * rnd2) * desviacion * media;
-                System.out.println("N " + N);
-                System.out.println("N2 " + N2);
+        for (int i = 0;i < this.N_muestra ;i++) {
+            
+                double rnd1 = Math.random();
+                double rnd2 = Math.random();
+                double N = Math.sqrt(-2 * Math.log(rnd1)) * Math.cos(2 * Math.PI * rnd2) * desviacion + media;
+                double N2 = Math.sqrt(-2 * Math.log(rnd1)) * Math.sin(2 * Math.PI * rnd2) * desviacion + media;
+                secuencia.add(N);
+                secuencia.add(N2);
 
 
-            }
+
+            
         }
         return secuencia;
 
 
 
     }
-
-
-
-
-
-
-
-
-
 
 }
