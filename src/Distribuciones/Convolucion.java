2e34179a3ad8fd937d76d0d8d93dce46d88772f0
Genaro Lambertucci <80863474+GenaroL@users.noreply.github.com>
2022-04-19 17:59:48 -0300

    se a;ade el metodo de convolucion a la clase normal, quedando unificado todo

--- src/Distribuciones/Convolucion.java
+++ src/Distribuciones/Convolucion.java
@@ -1,56 +1,97 @@
-package Distribuciones;
+package Generadores;
 
 import java.util.ArrayList;
-import java.lang.Math;
-import java.text.DecimalFormat;
 
+public class Normal{
 
-public class Convolucion {
     private double media;
     private double desviacion;
-    private ArrayList<Double> serie;
+    private int cantintervalos;
+    private int N_muestra;
+    private ArrayList<Double> secuencia;
 
     public double getMedia() {
-		return this.media;
-	}
+        return media;
+    }
 
-	public double getDesviacion() {
-		return this.desviacion;
-	}
+    public void setMedia(double media) {
+        this.media = media;
+    }
+
+    public double getDesviacion() {
+        return desviacion;
+    }
+
+    public void setDesviacion(double desviacion) {
+        this.desviacion = desviacion;
+    }
+
+
+    public int getCantintervalos() {
+        return cantintervalos;
+    }
+
+    public void setCantintervalos(int cantintervalos) {
+        this.cantintervalos = cantintervalos;
+    }
+
+    public int getN_muestra() {
+        return N_muestra;
+    }
+
+    public void setN_muestra(int n_muestra) {
+        N_muestra = n_muestra;
+    }
 
     public double getMinimo(){
-        ArrayList<Double> lista = this.serie;
+        ArrayList<Double> lista = this.secuencia;
         lista.sort(null);
         return lista.get(0);
     }
 
     public double getMaximo(){
-        ArrayList<Double> lista = this.serie;
+        ArrayList<Double> lista = this.secuencia;
         lista.sort(null);
         return lista.get(lista.size()-1);
     }
 
-	public ArrayList<Double> getSerie() {
-		return this.serie;
-	}
+    public ArrayList<Double> getSecuencia() {
+        return this.secuencia;
+    }
 
-    public double getMediaSerie(){
+    public double getMediaSecuencia(){
         double suma =0;
-        for (int i=0; i<this.serie.size(); i++){
-            suma += this.serie.get(i);
+        for (int i=0; i<this.secuencia.size(); i++){
+            suma += this.secuencia.get(i);
         }
-        return (suma/this.serie.size());
+        return (suma/this.secuencia.size());
+    }
+
+    public ArrayList<Double> generarSecuenciaBoxMuller(double N_muestra, double desviacion, double media){
+
+
+        for (int i = 0;i < this.N_muestra ;i++) {
+
+            double rnd1 = Math.random();
+            double rnd2 = Math.random();
+            double N = Math.sqrt(-2 * Math.log(rnd1)) * Math.cos(2 * Math.PI * rnd2) * desviacion + media;
+            double N2 = Math.sqrt(-2 * Math.log(rnd1)) * Math.sin(2 * Math.PI * rnd2) * desviacion + media;
+            this.secuencia.add(N);
+            this.secuencia.add(N2);
+        }
+        return secuencia;
+
     }
 
     public void generarConvolucion(double m, double d, double n){
         this.media = m;
         this.desviacion = d;
-        this.serie = new ArrayList<Double>();
+        this.secuencia = new ArrayList<Double>();
         for (int i=0; i< n; i++){
             double suma = this.generarRNDaux();
             double z = (((suma - 6) * this.desviacion) + this.media);
             z = this.truncarNumero(z);
-            this.serie.add(z);
+            this.secuencia.add(z);
         }
     }
 
@@ -70,5 +111,4 @@ public class Convolucion {
         String valor_truncado = df.format(valor).replace(",", ".");
         return Double.parseDouble(valor_truncado);
     }
-
 }
